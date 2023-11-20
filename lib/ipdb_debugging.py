1e5d6477586ce19e55d0be6726c9b4cad1c03feb
tray <onesimus.githua@student.moringaschool.com>
2023-11-21 02:19:58 +0300

    completed lab

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
+# lib/ipdb_debugging.py
 
 import ipdb
 
 def plus_two(num):
-    num + 2
+    num = num + 2  # Update the value of num by assigning the result back
+    # ipdb.set_trace()  # Drop into the debugger
     return num
